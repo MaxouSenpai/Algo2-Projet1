363b9237438194c3cf8a3e7d7162bb030c83fb87
MaxouSenpai <Maxime.Hauwaert@ulb.ac.be>
2018-10-19 21:28:45 +0200

    Deleting some comments + weights in labels

--- algo2_2018.py
+++ algo2_2018.py
@@ -38,16 +38,17 @@ class Tree(object):
     def show(self,figure=1):
         G1=nx.DiGraph()
         self.makeGraph(G1)
-        pos = hierarchy_pos(G1,self.name)
+        pos = hierarchy_pos(G1,self.name+" : "+str(self.weight))
         plt.figure(figure)
-        nx.draw_networkx_nodes(G1,pos,node_size=700)
-        nx.draw_networkx_edges(G1,pos,width=4)
-        nx.draw_networkx_labels(G1,pos,font_size=20,font_family='sans-serif')
+        nx.draw_networkx_nodes(G1,pos,node_size=1000)
+        nx.draw_networkx_edges(G1,pos,width=3,arrowstyle="<|-|>",arrowsize=20)
+        nx.draw_networkx_labels(G1,pos,font_size=10,font_family='sans-serif')
         plt.axis('off')
 
     def makeGraph(self,G):
         for child in self.children:
-            G.add_edge(self.name,child.name,weight=child.weight)
+            #G.add_edge(self.name,child.name,weight=child.weight)
+            G.add_edge(self.name + " : " + str(self.weight),child.name + " : " + str(child.weight))
             child.makeGraph(G)
 
 def hierarchy_pos(G, root, width=1, vert_gap = 0.2, vert_loc = 0, xcenter = 0.5,
@@ -66,8 +67,6 @@ def hierarchy_pos(G, root, width=1, vert_gap = 0.2, vert_loc = 0, xcenter = 0.5,
     else:
         pos[root] = (xcenter, vert_loc)
     neighbors = list(G.neighbors(root))
-    #if parent != None:   #this should be removed for directed graphs.
-    #    neighbors.remove(parent)  #if directed, then parent not in neighbors.
     if len(neighbors)!=0:
         dx = width/len(neighbors)
         nextx = xcenter - width/2 - dx/2
@@ -81,43 +80,10 @@ def hierarchy_pos(G, root, width=1, vert_gap = 0.2, vert_loc = 0, xcenter = 0.5,
 def max_subtree(t):
     c = copy.deepcopy(t)
     print(str(c.contribution()) + " ---> " + str(c))
-    """
-    G1=nx.DiGraph()
-    t.makeGraph(G1)
-    #pos = nx.circular_layout(G1)
-    pos = hierarchy_pos(G1,t.name)
-    print(pos)
-    plt.figure(1)
-    nx.draw_networkx_nodes(G1,pos,node_size=700)
-    nx.draw_networkx_edges(G1,pos,width=6)
-    nx.draw_networkx_labels(G1,pos,font_size=20,font_family='sans-serif')
-    plt.axis('off')
-    #plt.figure(1)
-    #nx.draw(G1)
-    """
-    t.show()
-    
-    """
-    G=nx.DiGraph()
-    c.makeGraph(G)
-    #pos = nx.circular_layout(G)
-    pos = hierarchy_pos(G,c.name)
-    plt.figure(2)
-    nx.draw_networkx_nodes(G,pos,node_size=700)
-    nx.draw_networkx_edges(G,pos,width=6)
-    nx.draw_networkx_labels(G,pos,font_size=20,font_family='sans-serif')
-    #nx.draw(G)
-    #labels = nx.get_edge_attributes(G,'weight')
-    #print(labels)
-    #labels = nx.get_node_attributes(G,'A')
-    #print(labels)
-    #nx.draw_networkx_edge_labels(G,pos,edge_label=labels)
-    #nx.draw_networkx_labels(G,pos,font_size=20,font_family='sans-serif')
-    plt.axis('off')
-    plt.savefig("weighted_graph.png") # save as png
-    plt.show() # display
-    """
-    c.show(2)
+
+    t.show("Before")
+
+    c.show("After")
 
     plt.show()
 
